4647523d8b4f21529e82db065708ffb859ec10e2
Brian Mathews <brian@wintr.us>
2016-01-15 09:01:19 -0800

    Simplify ipfs loader

--- app/loaders/ipfs.js
+++ app/loaders/ipfs.js
@@ -7,26 +7,26 @@ module.exports = {
   },
 
   load(uri) {
-    return new Promise((resolve, reject) => {
-      let link = uri
-      if (uri[0] !== '/') link = '/' + link // may be stripped in add
+    let link = uri
+    if (uri[0] !== '/') link = '/' + link // may be stripped in add
 
-      const local = 'localhost:8080' // todo: make this configurable
-      const gateway = 'gateway.ipfs.io'
+    const local = 'localhost:8080' // todo: make this configurable
+    const gateway = 'gateway.ipfs.io'
 
-      // first, try the local http gateway
-      let u = 'http://' + local + link
-      console.log('trying local ipfs gateway: ' + u)
-      httpLoader.load(u).catch(() => {
+    // first, try the local http gateway
+    let u = 'http://' + local + link
+    console.log('trying local ipfs gateway: ' + u)
+    return httpLoader.load(u)
+      .catch(() => {
         // error? ok try fuse... maybe the gateway's broken.
         console.log('trying mounted ipfs fs (just in case)')
-        return fileLoader.load(link).catch(() => {
-          // worst case, try global ipfs gateway.
-          u = 'http://' + gateway + link
-          console.log('trying local ipfs gateway: ' + u)
-          return httpLoader.load(u)
-        })
-      }).then(resolve).catch(reject)
-    })
+        return fileLoader.load(link)
+      })
+      .catch(() => {
+        // worst case, try global ipfs gateway.
+        u = 'http://' + gateway + link
+        console.log('trying local ipfs gateway: ' + u)
+        return httpLoader.load(u)
+      })
   }
 }
