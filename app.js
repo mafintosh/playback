a9781304d844924d549e9c5104f86dc61204407a
cesine <cesine@yahoo.com>
2016-02-15 20:51:02 -0500

    loadUrl is deprecated. Use loadURL instead.

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.on('ready', function () {
     transparent: true
   })
 
-  win.loadUrl('file://' + path.join(__dirname, 'index.html#' + JSON.stringify(process.argv.slice(2))))
+  win.loadURL('file://' + path.join(__dirname, 'index.html#' + JSON.stringify(process.argv.slice(2))))
 
   ipc.on('close', function () {
     app.quit()
